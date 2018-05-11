6ec2bed8c550e01618559c07e97455170e512fee
simalexan <alexander.simovic@gmail.com>
2018-05-11 20:46:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+## SNS -> Lambda -> S3
+
+#### Description
+This is a SNS Lambda subscriber that stores notification data into an S3 Bucket. 
+
+Written in Node.js. 
+
+It's a Nuts&Bolts application component for AWS Serverless Application Repository.
---
+++ index.js
@@ -0,0 +1,25 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const uuidv4 = require('uuid/v4');
+const parseSnsMessage = require('./parse-sns-event');
+const TOPIC_ARN = process.env.TOPIC_ARN;
+const BUCKET_NAME = process.env.BUCKET_NAME;
+
+exports.handler = (event) => {
+    let messages = parseSnsMessage(event);
+    return Promise.all(messages.map(saveToS3));
+};
+
+saveToS3 = (data) => {
+    let year = date.getFullYear(),
+        month = date.getMonth() + 1,
+        day = date.getDate(),
+        hash = uuidv4();
+
+    return s3.putObject({
+        Key: `${year}-${month}-${day}-${hash}`,
+        Bucket: BUCKET_NAME,
+        ContentType: 'application/json',
+        Body: JSON.stringify(data, null, 2)
+    }).promise();
+};
